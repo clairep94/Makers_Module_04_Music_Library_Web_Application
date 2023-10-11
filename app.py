41c925617fc5f52fd8fc75104511b3a89a3963ee
ClairePeng <clairepeng94@gmail.com>
2023-10-11 14:25:01 +0100

    project done up to first recorded challenge

--- app.py
+++ app.py
@@ -1,32 +1,47 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from lib.database_connection import get_flask_database_connection
+
+from lib.album import Album
+from lib.album_repository import AlbumRepository
+from lib.artist import Artist
+from lib.artist_repository import ArtistRepository
+
 
 # Create a new Flask app
 app = Flask(__name__)
 
 # == Your Routes Here ==
 
+# == HOMEPAGE ==
+@app.route('/', methods=['GET'])
+def home_page():
+    connection = get_flask_database_connection(app)
+    return render_template('homepage.html')
 
-# == Example Code Below ==
 
-# GET /emoji
-# Returns a smiley face in HTML
-# Try it:
-#   ; open http://localhost:5001/emoji
-@app.route('/emoji', methods=['GET'])
-def get_emoji():
-    # We use `render_template` to send the user the file `emoji.html`
-    # But first, it gets processed to look for placeholders like {{ emoji }}
-    # These placeholders are replaced with the values we pass in as arguments
-    return render_template('emoji.html', emoji=':)')
+# == ALBUMS ==
+@app.route('/albums/<int:id>', methods=['GET'])
+def get_album(id):
+    connection = get_flask_database_connection(app)
+    album_repository = AlbumRepository(connection)
+    album = album_repository.find(id)
+    return render_template('albums/show.html', album=album)
 
-# This imports some more example routes for you to see how they work
-# You can delete these lines if you don't need them.
-from example_routes import apply_example_routes
-apply_example_routes(app)
+@app.route('/albums', methods=['GET'])
+def get_albums():
+    connection = get_flask_database_connection(app)
+    album_repository = AlbumRepository(connection)
+    albums = album_repository.all() #switched to list of dictionaries with artist_name included
+    return render_template('albums/index.html', albums=albums)
 
-# == End Example Code ==
+@app.route('/albums/new', methods=['GET'])
+def new_album_page():
+    connection = get_flask_database_connection(app)
+    return render_template('albums/new.html')
+
+
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,19 +2,50 @@ from playwright.sync_api import Page, expect
 
 # Tests for your routes go here
 
-# === Example Code Below ===
+'''
+When we go to the homepage,
+We should get a link for All Albums and All Artists
+'''
+def test_home_page(db_connection, page, test_web_address):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/")
+    header_two_items = page.locator("h2")
 
-"""
-We can get an emoji from the /emoji page
-"""
-def test_get_emoji(page, test_web_address): # Note new parameters
-    # We load a virtual browser and navigate to the /emoji page
-    page.goto(f"http://{test_web_address}/emoji")
 
-    # We look at the <strong> tag
-    strong_tag = page.locator("strong")
 
-    # We assert that it has the text ":)"
-    expect(strong_tag).to_have_text(":)")
 
-# === End Example Code ===
+'''
+We can get Album title, 
+'''
+def test_get_albums(db_connection, page, test_web_address):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/albums")
+    list_items = page.locator("li")
+    expect(list_items).to_have_text([
+        "Doolittle (1989) by Pixies",
+        "Surfer Rosa (1988) by Pixies",
+        "Waterloo (1974) by ABBA",
+        "Super Trouper (1980) by ABBA",
+        "Bossanova (1990) by Pixies",
+        "Lover (2019) by Taylor Swift",
+        "Folklore (2020) by Taylor Swift",
+        "I Put a Spell on You (1965) by Nina Simone",
+        "Baltimore (1978) by Nina Simone",
+        "Here Comes the Sun (1971) by Nina Simone",
+        "Fodder on My Wings (1982) by Nina Simone",
+        "Ring Ring (1973) by ABBA"
+    ])
+
+
+def test_get_album(db_connection, page, test_web_address):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/albums")
+    page.click("text=Doolittle")
+    title_element = page.locator(".t-title")
+    expect(title_element).to_have_text("Title: Doolittle")
+    release_year = page.locator(".t-release_year")
+    expect(release_year).to_have_text("Release year: 1989")
+    artist_name = page.locator(".t-artist_name")
+    expect(artist_name).to_have_text("Artist: Pixies")
+    
+    
